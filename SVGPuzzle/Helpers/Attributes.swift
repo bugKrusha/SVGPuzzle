5d1b6b3432f5f0ef55578ec5d4f2d11d7776ea13
Jon-Tait Beason <jt.beason@icloud.com>
2017-11-02 22:22:00 -0700

    Add svg document manager.

---
+++ SVGPuzzle/Helpers/Attributes.swift
@@ -0,0 +1,15 @@
+//
+//  Attributes.swift
+//  SVGPuzzle
+//
+//  Created by Jon-Tait Beason on 11/2/17.
+//  Copyright © 2017 Jon-Tait Beason. All rights reserved.
+//
+
+import Foundation
+
+struct Attributes {
+    static let path = "path"
+    static let dPath = "d"
+    static let _class = "class"
+}
